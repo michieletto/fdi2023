5105008b0b4c2a731c8ca875c1aa086d86133b20
Stefano Michieletto <stefano.michieletto@unipd.it>
2024-01-15 11:41:32 +0100

    Lezione 26: Esercizi su liste e ricorsione

---
+++ 20240115/caesar.c
@@ -0,0 +1,12 @@
+// caesar.c
+// Include le librerie necessarie
+#include "caesar.h"         // Libreria con le funzioni
+
+/**
+ * Crea un nodo a partire dal valore in input
+ * @param [in] value valore da inserire come dato nel nodo
+ * @return puntatore al nodo creato
+**/
+struct CharNode* new_node(char value) {
+
+}
