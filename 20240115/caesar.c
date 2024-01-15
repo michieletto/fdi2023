6858291c9464e8b3f780d3aab69d51f4a8479ecd
Stefano Michieletto <stefano.michieletto@unipd.it>
2024-01-15 11:42:48 +0100

    Lezione 26: Esercizi su liste e ricorsione

--- 20240115/caesar.c
+++ 20240115/caesar.c
@@ -10,3 +10,18 @@
 struct CharNode* new_node(char value) {
 
 }
+
+/**
+ * Crea una lista di caratteri (solo minuscoli) cifrata con il cifrario di Cesare
+ * a partire dalla lista di caratteri in input e dalla chiave
+ * @param [in] head puntatore alla testa della lista di caratteri
+ * @param [in] key chiave di cifratura
+ * @return puntatore alla testa della lista di caratteri cifrata
+ * @note il cifrario di Cesare è una cifratura monoalfabetica
+ *       che consiste nello spostare ogni lettera del messaggio
+ *       di un numero di posizioni pari alla chiave
+ *      (es. chiave = 3, a -> d, b -> e, ..., z -> c)
+**/
+struct CharNode* caesar_cipher(struct CharNode *head, int key) {
+    
+}
